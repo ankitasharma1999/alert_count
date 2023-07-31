35b9273f44b607f03d92d113f87ccd79a437cd59
ankitasharma1999 <98891495+ankitasharma1999@users.noreply.github.com>
2023-07-31 15:19:40 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,48 +1,44 @@
-resource "newrelic_alert_policy" "foo" {
-  name = "foo"
-}
-
-locals {
-  condition_keys = keys(var.condition)
+resource "newrelic_alert_policy" "count_alert" {
+  name = "count_alert"
 }
 
 resource "newrelic_nrql_alert_condition" "foo" {
-  count = length(local.condition_keys)
+  count=length(var.alert_loop)
 
-  account_id                     = var.condition[local.condition_keys[count.index]].account_id
-  policy_id                      = newrelic_alert_policy.foo.id
-  type                           = var.condition[local.condition_keys[count.index]].type
-  name                           = var.condition[local.condition_keys[count.index]].name
-  description                    = var.condition[local.condition_keys[count.index]].description
-  runbook_url                    = var.condition[local.condition_keys[count.index]].runbook_url
-  enabled                        = var.condition[local.condition_keys[count.index]].enabled
-  violation_time_limit_seconds   = var.condition[local.condition_keys[count.index]].violation_time_limit_seconds
-  fill_option                    = var.condition[local.condition_keys[count.index]].fill_option
-  fill_value                     = var.condition[local.condition_keys[count.index]].fill_value
-  aggregation_window             = var.condition[local.condition_keys[count.index]].aggregation_window
-  aggregation_method             = var.condition[local.condition_keys[count.index]].aggregation_method
-  aggregation_delay              = var.condition[local.condition_keys[count.index]].aggregation_delay
-  expiration_duration            = var.condition[local.condition_keys[count.index]].expiration_duration
-  open_violation_on_expiration   = var.condition[local.condition_keys[count.index]].open_violation_on_expiration
-  close_violations_on_expiration = var.condition[local.condition_keys[count.index]].close_violations_on_expiration
-  slide_by                       = var.condition[local.condition_keys[count.index]].slide_by
-  baseline_direction             = var.condition[local.condition_keys[count.index]].baseline_direction
+  account_id                     = var.alert_loop[count.index].account_id
+  policy_id                      = newrelic_alert_policy.count_alert.id
+  type                           = var.alert_loop[count.index].type
+  name                           = var.alert_loop[count.index].name
+  description                    = var.alert_loop[count.index].description
+  runbook_url                    = var.alert_loop[count.index].runbook_url
+  enabled                        = var.alert_loop[count.index].enabled
+  violation_time_limit_seconds   = var.alert_loop[count.index].violation_time_limit_seconds
+  fill_option                    = var.alert_loop[count.index].fill_option
+  fill_value                     = var.alert_loop[count.index].fill_value
+  aggregation_window             = var.alert_loop[count.index].aggregation_window
+  aggregation_method             = var.alert_loop[count.index].aggregation_method
+  aggregation_delay              = var.alert_loop[count.index].aggregation_delay
+  expiration_duration            = var.alert_loop[count.index].expiration_duration
+  open_violation_on_expiration   = var.alert_loop[count.index].open_violation_on_expiration
+  close_violations_on_expiration = var.alert_loop[count.index].close_violations_on_expiration
+  slide_by                       = var.alert_loop[count.index].slide_by
+
 
   nrql {
-    query = var.condition[local.condition_keys[count.index]].query
+    query = var.alert_loop[count.index].nrql
   }
 
   critical {
-    operator              = var.condition[local.condition_keys[count.index]].operator
-    threshold             = var.condition[local.condition_keys[count.index]].threshold
-    threshold_duration    = var.condition[local.condition_keys[count.index]].threshold_duration
-    threshold_occurrences = var.condition[local.condition_keys[count.index]].threshold_occurrences
+    operator              = var.alert_loop[count.index].critical.operator
+    threshold             = var.alert_loop[count.index].critical.threshold
+    threshold_duration    = var.alert_loop[count.index].critical.threshold_duration
+    threshold_occurrences = var.alert_loop[count.index].critical.threshold_occurrences
   }
 
   warning {
-    operator              = var.condition[local.condition_keys[count.index]].operator
-    threshold             = var.condition[local.condition_keys[count.index]].threshold
-    threshold_duration    = var.condition[local.condition_keys[count.index]].threshold_duration
-    threshold_occurrences = var.condition[local.condition_keys[count.index]].threshold_occurrences
+    operator              = var.alert_loop[count.index].warning.operator
+    threshold             = var.alert_loop[count.index].warning.threshold
+    threshold_duration    = var.alert_loop[count.index].warning.threshold_duration
+    threshold_occurrences = var.alert_loop[count.index].warning.threshold_occurrences
   }
 }
